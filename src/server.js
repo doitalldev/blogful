37ec4f7e5bc7bec4a0220ca6d8b646e6a35f9bcd
Jonathan Clark <jonclark822@gmail.com>
2020-06-15 14:00:12 -0700

    added relationships

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const knex = require('knex');
+const app = require('./app');
+
+const { PORT, NODE_ENV, DB_URL } = require('./config');
+
+const db = knex({
+    client: 'pg',
+    connection: DB_URL,
+});
+
+app.set('db', db);
+
+app.listen(PORT, () => {
+    console.log(`Server listening in ${NODE_ENV} at http://localhost:${PORT}`);
+});
